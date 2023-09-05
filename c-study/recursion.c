52a69a88fdf629d17a4882dda27e3ecf4449f423
Mahesh Vaidya <forvaidya@gmail.com>
2023-09-05 20:40:54 +0530

    Create recursion.c

---
+++ c-study/recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int facto(int) ;
+
+int facto(int input) {
+
+  if ( input < 2 ) {
+     return(1);
+  } else {
+
+     return (input * facto(input -1)) ;
+}
+
+
+}
+
+int main() {
+
+  int k = 4;
+  printf("%d", facto(k));
+  return(0) ;
